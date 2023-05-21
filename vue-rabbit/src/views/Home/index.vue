9f93ec541125490bc6ecc1d2944f1d8e8787301e
ColorfulPipiEel <1640380196@qq.com>
2023-05-21 19:06:22 +0800

    router基础封装

---
+++ vue-rabbit/src/views/Home/index.vue
@@ -0,0 +1,11 @@
+<!--
+ * @Description: home页
+ * @Author: Ben
+ * @Date: 2023-05-21 19:01:45
+ * @LastEditors: Ben
+ * @LastEditTime: 2023-05-21 19:02:01
+ * @FilePath: \src\views\Home\index.vue
+-->
+<template>
+  <div>我是home页</div>
+</template>
---
+++ vue-rabbit/src/views/Layout/index.vue
@@ -0,0 +1,15 @@
+<!--
+ * @Description: 首页
+ * @Author: Ben
+ * @Date: 2023-05-21 18:30:07
+ * @LastEditors: Ben
+ * @LastEditTime: 2023-05-21 18:30:38
+ * @FilePath: \src\views\Layout\index.vue
+-->
+<template>
+  <div>
+    我是首页
+    <!-- 二级路由出口 -->
+    <RouterView />
+  </div>
+</template>
