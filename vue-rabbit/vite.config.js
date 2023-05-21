be08473e15f81dada0353b7562eae301e6069ca6
ColorfulPipiEel <1640380196@qq.com>
2023-05-21 17:50:56 +0800

    elementPlus自动按需引入配置

--- vue-rabbit/vite.config.js
+++ vue-rabbit/vite.config.js
@@ -1,11 +1,33 @@
+/*
+ * @Description: 
+ * @Author: Ben
+ * @Date: 2023-05-21 17:04:47
+ * @LastEditors: Ben
+ * @LastEditTime: 2023-05-21 17:46:07
+ * @FilePath: \vite.config.js
+ */
 import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
+// ElementPlus按需引入
+import AutoImport from 'unplugin-auto-import/vite'
+import Components from 'unplugin-vue-components/vite'
+import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
+
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    // ...ElementPlus按需引入
+    AutoImport({
+      resolvers: [ElementPlusResolver()],
+    }),
+    Components({
+      resolvers: [ElementPlusResolver()],
+    }),
+  ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
