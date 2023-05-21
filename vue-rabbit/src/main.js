c1c1b7406483990857735b25c4f94377a41ad247
ColorfulPipiEel <1640380196@qq.com>
2023-05-21 18:27:25 +0800

    axios基础封装

--- vue-rabbit/src/main.js
+++ vue-rabbit/src/main.js
@@ -1,3 +1,4 @@
+
 import './assets/main.css'
 
 import { createApp } from 'vue'
@@ -6,6 +7,11 @@ import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
 
+// 测试接口函数
+import {getCategory} from './apis/testAPI'
+getCategory().then(res => {
+  console.log(res)
+})
 const app = createApp(App)
 
 app.use(createPinia())
